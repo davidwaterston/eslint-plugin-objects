4c552e7aaf58fc79cbaac152889e98ca6468af4f
Leonard Kinday <leonard@kinday.ru>
2016-11-29 13:45:30 +0100

    Add object literal shorthands to tests

--- test/rules/no-object-properties-one-line.js
+++ test/rules/no-object-properties-one-line.js
@@ -8,26 +8,26 @@ eslintTester.run('no-object-properties-one-line', rule, {
     valid: [
         {code: 'var a = {};'},
         {code: 'var a = {b: 1};'},
-        {code: 'var a = {b: 1,\n         c: 2};'},
-        {code: 'var a = {\n    b: 1,\n    c: 2};'},
-        {code: 'var a = {\n    b: 1,\n    c: 2\n};'},
+        {code: 'var a = {b: 1,\n         c: 2,\n         d};'},
+        {code: 'var a = {\n    b: 1,\n    c: 2,\n    d};'},
+        {code: 'var a = {\n    b: 1,\n    c: 2,\n    d\n};'},
         {code: 'var a = {a: 1, b: 2}', options: [{properties: 2}]}
     ],
     invalid: [
         {
-            code: 'var a = {b: 1, c: 2};',
+            code: 'var a = {b: 1, c: 2, d};',
             errors: [
                 {message: 'multiple object properties on one line'}
             ]
         },
         {
-            code: 'var a = {\n    b: 1, c: 2,\n    d: 3\n};',
+            code: 'var a = {\n    b: 1, c: 2,\n    d: 3,\n    e\n};',
             errors: [
                 {message: 'multiple object properties on one line'}
             ]
         },
         {
-            code: 'var a = {a: 1, b: 2, c: 3, d: 4}',
+            code: 'var a = {a: 1, b: 2, c: 3, d: 4, e}',
             args: [2, {properties: 2}],
             errors: [
                 {message: 'multiple object properties on one line'}
