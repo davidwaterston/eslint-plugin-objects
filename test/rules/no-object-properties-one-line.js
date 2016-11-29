89b09af3cfa5f671456d726e5a7ffd8d2be7e915
Leonard Kinday <leonard@kinday.ru>
2016-11-29 14:05:02 +0100

    Add support for object destructuring assignments

--- test/rules/no-object-properties-one-line.js
+++ test/rules/no-object-properties-one-line.js
@@ -11,7 +11,8 @@ eslintTester.run('no-object-properties-one-line', rule, {
         {code: 'var a = {b: 1,\n         c: 2,\n         d};'},
         {code: 'var a = {\n    b: 1,\n    c: 2,\n    d};'},
         {code: 'var a = {\n    b: 1,\n    c: 2,\n    d\n};'},
-        {code: 'var a = {a: 1, b: 2}', options: [{properties: 2}]}
+        {code: 'var a = {a: 1, b: 2}', options: [{properties: 2}]},
+        {code: 'const {\n    a,\n    b,\n    c\n} = {\n    a: 1,\n    b: 2,\n    c: 3\n}'}
     ],
     invalid: [
         {
@@ -29,6 +30,12 @@ eslintTester.run('no-object-properties-one-line', rule, {
         {
             code: 'var a = {a: 1, b: 2, c: 3, d: 4, e}',
             args: [2, {properties: 2}],
+            errors: [
+                {message: 'multiple object properties on one line'}
+            ]
+        },
+        {
+            code: 'const {a, b, c} = {\n    a: 1,\n    b: 2,\n    c: 3\n}',
             errors: [
                 {message: 'multiple object properties on one line'}
             ]
